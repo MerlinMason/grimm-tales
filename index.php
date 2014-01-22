70765f6561c0de81091195a51730b5dbd3d0fefe
Eightarmedpet <hi@eightarmedpet.com>
2014-01-22 07:47:42 +0000

    Content update and tweaks

--- index.php
+++ index.php
@@ -2,26 +2,53 @@
 
 <section id="grimm-tales" class="page-section dark first">
     <h1 class="main-heading light text-center">Grimm Tales</h1>
+        <div class="container">
+             <p>Announcing the world debut of a never-before-experienced immersive storytelling event – a theatrical reimagining of award-winning author Philip Pullman’s Grimm Tales for Young and Old in Shoreditch Town Hall, London.
+                 </br>
+                We invite you to experience a handful of classic fairytales in the flesh. From Little Red Riding Hood to Rapunzel, you’ll brush shoulders with unforgettable characters from Philip Pullman’s book, in a theatre production that will enchant and inspire children and adults of all ages. This is an Easter experience full of unexpected twists and turns, well suited to adults with a keen sense of curiosity and their brave children.
+                </br>
+                Infused with storytelling magic, immersive action and raw excitement, this is a way to see these classic fairytales like never before. </p>
+
+               <div class="row">
+
+                <div class="col-sm-4">
+                    <p class="pull">‘He (Philip Pullman) makes us fall in love all over again with the immortal tales of the Brothers Grimm.’ 
+                        </p><p class="credit">The New York Timesfor</p>
+                </div>
+
+                 <div class="col-sm-4">
+                    <p class="pull">‘He (Philip Pullman) makes us fall in love all over again with the immortal tales of the Brothers Grimm.’ 
+                        </p><p class="credit">The New York Timesfor</p>
+                </div>
+
+                 <div class="col-sm-4">
+                    <p class="pull">‘He (Philip Pullman) makes us fall in love all over again with the immortal tales of the Brothers Grimm.’ 
+                        </p><p class="credit">The New York Timesfor</p>
+                </div>
+
+            </div>  
+        </div>  
 </section>
 
 <section id="about" class="page-section light">
     <div class="container">
         <h1 class="main-heading dark text-center">About The Show</h1>
-
         <div class="row">
-            <div class="col-sm-9">
+            <div class="col-sm-12">
                 <div class="fitvid-wrapper">
                     <iframe class="player-frame" src="//player.vimeo.com/video/78050478?title=0&byline=0&portrait=0&color=ffffff&loop=0" frameborder="0" width="640" height="360"></iframe>
                 </div>
             </div>
-            <div class="col-sm-3">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, nobis.</p>
-                <h2 class="sub-heading">Credits:</h2>
-                <dl>
-                    <dt>title</dt>
-                    <dd>person</dd>
-                </dl>
-            </div>
+        </div>
+            
+            <div class="row">
+                <div class="col-sm-8">
+                    <p class= "body-dark">We are proud to collaborate with master storyteller Philip Pullman CBE to bring these classic tales quite literally to life through immersive theatre. Pullman is one of only nine authors in the world to have sold over 1 million books and his 2012 retelling of 50 of the Grimm Brothers’ Fairytales brought these classic stories to new generations of wide-eyed readers. </p>
+                </div>
+                <div class="col-sm-4">
+                     <p class= "body-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+                </div>
+            </div>    
         </div>
     </div>
 </section>
@@ -29,6 +56,42 @@
 <section id="the-stories" class="page-section dark">
     <div class="container">
         <h1 class="main-heading light text-center">The Six Tales</h1>
+        <div class="row">
+
+            <div class="col-sm-4">
+                <h2 class="sub-heading">General</h2> 
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+            </div>
+
+            <div class="col-sm-4">
+                <h2 class="sub-heading">General</h2>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+            </div>
+
+            <div class="col-sm-4">
+                <h2 class="sub-heading">Tickets</h2>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+            </div>
+        </div>    
+
+        <div class="row">
+
+            <div class="col-sm-4">
+                <h2 class="sub-heading">General</h2>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+            </div>
+
+            <div class="col-sm-4">
+                <h2 class="sub-heading">General</h2>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+            </div>
+
+            <div class="col-sm-4">
+                <h2 class="sub-heading">Tickets</h2>
+                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+            </div>
+
+        </div>
     </div>
 </section>
 
@@ -145,22 +208,22 @@
             <div class="col-sm-3">
                 <img src="http://placehold.it/260x260" alt="" class="clipped">
                 <h2 class="sub-heading">Type</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, qui!</p>
+                <p class= "body-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, qui!</p>
             </div>
             <div class="col-sm-3">
                 <img src="http://placehold.it/260x260" alt="" class="clipped">
                 <h2 class="sub-heading">Type</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, est!</p>
+                <p class= "body-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde, est!</p>
             </div>
             <div class="col-sm-3">
                 <img src="http://placehold.it/260x260" alt="" class="clipped">
                 <h2 class="sub-heading">Type</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
+                <p class= "body-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus, vel.</p>
             </div>
             <div class="col-sm-3">
                 <img src="http://placehold.it/260x260" alt="" class="clipped">
                 <h2 class="sub-heading">Type</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, omnis.</p>
+                <p class= "body-dark">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro, omnis.</p>
             </div>
         </div>
     </div>
